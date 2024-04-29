9d2dd5e85d4cfcaf9183d123362bc6ef77354a94
David Molina <dmolina@bakerripley.org>
2024-04-29 11:19:36 -0500

    change Exception.cs to LogException.cs

---
+++ Models/LogException.cs
@@ -0,0 +1,10 @@
+
+namespace LogHunter
+{
+    public class LogException
+    {
+        public string? Type { get; set; }
+        public string? Message { get; set; }
+        public string? Stacktrace { get; set; }
+    }
+}
