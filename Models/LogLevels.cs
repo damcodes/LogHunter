d967090433422b875517329c2e963ddb5f98da23
David Molina <dmolina@bakerripley.org>
2024-05-03 13:44:07 -0500

    #1899 - change CRITICAL to FATAL to match NLog logging output

--- Models/LogLevels.cs
+++ Models/LogLevels.cs
@@ -8,9 +8,9 @@ namespace LogHunter
         public static LogLevels Info { get => new("INFO"); }
         public static LogLevels Trace { get => new("TRACE"); }
         public static LogLevels Error { get => new("ERROR"); }
-        public static LogLevels Critical { get => new("CRITICAL"); }
+        public static LogLevels Fatal { get => new("FATAL"); }
         public static LogLevels Warn { get => new("WARN"); }
-        public static readonly string[] Levels = [Info.Value, Trace.Value, Error.Value, Critical.Value, Warn.Value];
+        public static readonly string[] Levels = [Info.Value, Trace.Value, Error.Value, Fatal.Value, Warn.Value];
         public static new string ToString() => string.Join("\n", Levels.Select((level, i) => $"{i + 1}) {level}"));
     }
 }
